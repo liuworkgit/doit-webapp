6244354b40839a4457fe331a4e0f339d87ba9b47
moses21 <moses21@student.ubc.ca>
2024-08-11 12:40:30 -0700

    Added new style and css loaders to dev webpack config.

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -2,8 +2,19 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
     entry: './src/index.js',
+
+    mode: 'development',
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
