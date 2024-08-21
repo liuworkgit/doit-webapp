be58bbff9feb587e9f1c95e8fc3bc4e70ece2b62
moses21 <moses21@student.ubc.ca>
2024-08-20 20:26:07 -0700

    Added new Notes class.

---
+++ src/scripts/notes.js
@@ -0,0 +1,31 @@
+'use strict'
+
+/**
+ * @class Notes
+ * 
+ * A representation of a task's notes.
+ */
+class Notes {
+    /**
+     * Constructor for a Notes object.
+     * @param {String} t - The text of the note.
+     */
+    constructor(t) {};
+
+    /**
+     * Clears the note's text.
+     */
+    clearText() {};
+
+    /**
+     * Getters and Setters
+     */
+    /**
+     * @returns {String} The text of the note.
+     */
+    get getText() {};
+    /**
+     * @param {String} t - New text to be set.
+     */
+    set setText(t) {};
+};
