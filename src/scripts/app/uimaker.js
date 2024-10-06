c8bb52d3f25e99c54de0126ab0c0fcb0f76d58b0
moses21 <moses21@student.ubc.ca>
2024-10-05 18:41:36 -0700

    Wrote signatures for UIMaker methods.

--- src/scripts/app/uimaker.js
+++ src/scripts/app/uimaker.js
@@ -5,4 +5,29 @@
  * 
  * Represents a module that renders the UI.
  */
-export default class UIMaker {};
+export default class UIMaker {
+    /**
+     * Constructor for UIMaker.
+     */
+    constructor() {}
+
+    /**
+     * Creates a new Project Bar.
+     */
+    projBar() {}
+
+    /**
+     * Creates a new Project Editor.
+     */
+    projEditor() {}
+
+    /**
+     * Creates a new Task Bar.
+     */
+    taskBar() {}
+
+    /**
+     * Creates a new Task Editor.
+     */
+    taskEditor() {}
+};
