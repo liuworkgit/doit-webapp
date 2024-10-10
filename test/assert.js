bbe00f256d6a9d137c9e6c51c62eda82a01431da
moses21 <moses21@student.ubc.ca>
2024-10-09 19:44:43 -0700

    Finished implementing Assert class.

--- test/assert.js
+++ test/assert.js
@@ -5,25 +5,57 @@
  * 
  * A collection of assertions and methods to 
  * use in the test suites.
+ * 
+ * Assert allows for global access via a single object.
+ * 
+ * Code referenced from https://www.patterns.dev/vanilla/singleton-pattern/
  */
-export default class Assert {
+let instance;
+class Assert {
     /**
      * Constructor for Assert.
+     * @throws Instance already exists errors
      */
-    constructor() {}
+    constructor() {
+        if (instance) {
+            throw new Error('An instance of Assert already exists.');
+        };
+        instance = this;
+    };
+
+    /**
+     * Returns the single instance of Assert.
+     * @returns Assert
+     */
+    getInstance() {
+        return this;
+    }
 
     /**
      * Asserts if the given value is true.
      * @param {boolean} value - Value to check.
+     * @throws Incorrect type error
      * @returns boolean
      */
-    isTrue(value) {};
+    isTrue(value) {
+        if (typeof value !== boolean) {
+            throw new Error('value must be a boolean.');
+        }
+        return value === true;
+    };
 
     /**
      * Asserts if the given value is false.
      * @param {boolean} value - value to check.
+     * @throws Incorrect type error
+     * @returns boolean
      */
-    isFalse(value) {};
+    isFalse(value) {
+        if (typeof value !== boolean) {
+            throw new Error('value must be a boolean');
+        }
+        return value === false;
+    };
 
     /**
      * Asserts if the given values are equal.
@@ -31,5 +63,14 @@ export default class Assert {
      * @param {*} value2 - second value to compare.
      * @returns boolean
      */
-    isEqual(value1, value2) {};
+    isEqual(value1, value2) {
+        if (typeof value1 !== typeof value2) {
+            throw new Error('value1 and value2 are different types.');
+        }
+        return value1 === value2;
+    };
 };
+
+// prevent modification by consuming code,
+const assert = Object.freeze(new Assert());
+export default assert;
